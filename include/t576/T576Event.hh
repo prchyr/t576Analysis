1eec210b53b342a41fee222443a2d2bc2e5e3727
prchyr <prchyr@gmail.com>
2019-04-02 11:00:52 -0400

    figuring out surf positions

--- include/t576/T576Event.hh
+++ include/t576/T576Event.hh
@@ -199,9 +199,9 @@ public:
       }
     }
     //these are measured in feet.
-    //double cableLengths[12]={20., 10., 0., 0., 0., 0., 0., 0., 10., 20., 20., 50.};
+    double cableLengths[12]={20., 20., 10., 0., 0., 0., 0., 0., 0., 10.,  20., 50.};
     
-    double cableLengths[12]={50., 20., 20., 10., 0., 0., 0., 0., 0., 0., 10., 20.};
+    //double cableLengths[12]={50., 20., 20., 10., 0., 0., 0., 0., 0., 0., 10., 20.};
     double velocityFactor=-.82;//from Krijn and Kian, negative for math reasons
     
     TVector3 pos[12];
