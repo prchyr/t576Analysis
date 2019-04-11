3a52c9e760fda6b07f603b3e14546325e8d630a7
prchyr <prchyr@gmail.com>
2019-04-11 13:15:14 -0400

    expanding tutil

--- src/TUtil.cc
+++ src/TUtil.cc
@@ -2018,9 +2018,9 @@ void TUtil::set2DPalette(){
   const Int_t rgb = 5;
   const Int_t N = 255;
   
-  double red[]=     {0.90, 0.70, 1.00, 0.00, 0.00};
-  double green[]= {0.00, 0.30, 1.00, 0.40, 0.00};
-  double blue[]=  {0.00, 0.00, 1.00, 0.70, 0.90};
+  double red[]=     {0.90, 0.70, 0.80, 0.00, 0.00};
+  double green[]= {0.00, 0.40, 0.80, 0.40, 0.00};
+  double blue[]=  {0.00, 0.30, 1.00, 0.40, 0.00};
   double stops[]={0.00, 0.25, 0.50, 0.75, 1.00};
 
   TColor::CreateGradientColorTable(rgb, stops, red, green, blue, N);
