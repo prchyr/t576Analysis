c1d31635a8ff84819aa2fc4245ab26255e2c4f01
prchyr <prchyr@gmail.com>
2019-04-25 20:00:35 -0400

    modified the interpolation routines to be more flexible

--- src/T576Event.cc
+++ src/T576Event.cc
@@ -503,7 +503,7 @@ int T576Event::loadSurfEvent(int event, bool remove_dc_offset){
     }
     TGraph * graph=new TGraph(len, TUtil::makeIndices(len, 1./3.2, surf->delays[i]), surf->dat[i]);
 
-    TGraph *grChunk=TUtil::getChunkOfGraph(graph, 0, 250);
+    TGraph *grChunk=graph;//TUtil::getChunkOfGraph(graph, 0, 250);
     if(remove_dc_offset==true){
       TUtil::removeMeanInPlace(grChunk, 0., 30.);
     }
