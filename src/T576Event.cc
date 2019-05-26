26b55a34c709349e7736442818def59c12bf89c4
prchyr <prchyr@gmail.com>
2019-05-26 14:44:09 -0400

    fixing surf mem leak

--- src/T576Event.cc
+++ src/T576Event.cc
@@ -481,8 +481,8 @@ int T576Event::loadSurfEvent(int event, bool remove_dc_offset){
 
 
     cnpy::NpyArray  fSurfDataArray = fDataset["data"];
-    //    auto temp=(short*) calloc(60000000, sizeof(short));
-    auto temp=fSurfDataArray.data<short>();
+        auto temp=(short*) calloc(60000000, sizeof(short));
+    //auto temp=fSurfDataArray.data<short>();
     //    fSurfData=fSurfDataArray.data<short>();
     memcpy(fSurfData, temp, fSurfDataArray.num_bytes());
     //    free(temp);
