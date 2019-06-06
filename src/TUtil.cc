a8619aad80c16572b9a1310dce5cc7328879705b
prchyr <prchyr@gmail.com>
2019-06-06 16:07:59 -0400

    expanding TUtil

--- src/TUtil.cc
+++ src/TUtil.cc
@@ -1771,7 +1771,7 @@ TGraph * TUtil::makeCW(double freq,  double amp, double t_min, double t_max, dou
   double dt=1./GSs;
   double t=t_min;
   while(t<t_max){
-    double temp=amp*sin(2.*pi*freq*t - phase);
+    double temp=amp*sin(2.*pi*freq*t + phase);
     oG->SetPoint(oG->GetN(), t, temp);
     t+=dt;
   }
